04e0550b23d7f86840406f506faeb7716752de60
vakor03 <myksv200@gmail.com>
2020-12-02 17:30:57 +0300

    Lab 5

---
+++ Lab 5/Lab 5/Lab 5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Lab_5
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Input your n: ");
+            int n = Int32.Parse(Console.ReadLine());
+            
+            int maxSumOfDivs = 1;
+            int numWithMaxDivs = 1;
+            
+            for (int i = 1; i <= n; i++)
+            {
+                int sumOfDivs = 0;
+                for (int j = 1; j <= i; j++)
+                {
+                    if(i % j == 0) sumOfDivs += j;
+                }
+                Console.WriteLine($"Number {i} has the sum of divs: {sumOfDivs}");
+                
+                if (sumOfDivs > maxSumOfDivs)
+                {
+                    maxSumOfDivs = sumOfDivs;
+                    numWithMaxDivs = i;
+                }
+            }
+            
+            Console.WriteLine();
+            Console.WriteLine($"Number {numWithMaxDivs} has the max sum: {maxSumOfDivs}");
+            
+            
+            
+        }
+    }
+}
