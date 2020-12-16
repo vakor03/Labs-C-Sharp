bfc94e69d251ff8cf49d0458c2f3b0780bb2bfea
vakor03 <71404364+vakor03@users.noreply.github.com>
2020-12-16 16:12:43 +0300

    Lab 7,8

---
+++ Lab 6/Lab 6/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input your eps: ");
+            double eps = double.Parse(Console.ReadLine());
+            //double eps = 0.01;
+            
+            for (double i = 0.5; i <= 3; i+=0.5)
+            {
+                Console.WriteLine($"x = {i}, Ln = {FindY(i, eps)}");
+            }
+        }
+        
+        private static double Ln(double x, double eps)
+        {
+            if (x == 0) return  Int32.MaxValue;
+
+            double currEl = x - 1;
+            int n = 2;
+            double sum = currEl;
+            do
+            {
+                currEl = Math.Pow(-1, n-1) * Math.Pow(x - 1, n) / (double) n;
+                sum += currEl;
+                n++;
+            } while (Math.Abs(currEl) > eps);
+            return sum; 
+        }
+
+        private static double FindY(double x, double eps)
+        {
+            
+            double res = 0;
+            if (x < 2){
+                res = Ln(x, eps) + Ln(x/2, eps);
+            }
+            else if (x >= 2)
+                res = Ln(x/2 - 1, eps);
+            return res;
+        }
+    }
+    
+    
+}
---
+++ Lab 7/Lab 7/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace Lab_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input number of elements: ");
+            int n = Int32.Parse(Console.ReadLine());
+            int[] array = CreateRandomArray(n);
+            Console.WriteLine($"Fir array: {String.Join(", ", array)}");
+            
+            int[] secondArray = CreateSecondArray(array);
+            Console.WriteLine($"Sec array: {String.Join(", ", secondArray)}");
+            
+            int d = FindD(array, secondArray);
+            ChangeFirstArray(ref array, d);
+            
+            Console.WriteLine($"d = {d}");
+            Console.WriteLine($"Changed array: {String.Join(", ", array)}");
+           
+        }
+
+        private static int[] CreateRandomArray(int n)
+        { 
+            int[] array = new int[n];
+            Random rand = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next(0, 50);
+            }
+            return array;
+        }
+
+        private static int FindMax(int[] array)
+        {
+            int max = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (max < array[i]) max = array[i];
+            }
+            return max;
+        }
+
+        private static int[] CreateSecondArray(int[] array)
+        {
+            int max = FindMax(array);
+            Console.WriteLine($"Max el = {max}");
+            int[] secondArray = new int[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                secondArray[i] = max - array[i];
+            }
+            return secondArray;
+        }
+
+        private static int FindD(int[] array1, int[] array2)
+        {
+            double d = 0;
+            for (int i = 0; i < array1.Length; i++)
+            {
+                d += Math.Pow(array1[i] - array2[i], 2);
+            }
+            return (int) d;
+        }
+
+        private static void ChangeFirstArray(ref int[] array, int d)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i]%7==0) array[i] += d;
+            }
+        }
+            
+    }
+}
